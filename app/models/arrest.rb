43666aef08cfe95f269f9c73bbfccebb69062c42
William Zembricki <wzembricki@gmail.com>
2021-06-02 23:01:47 -0700

    Graph labels working

--- app/models/arrest.rb
+++ app/models/arrest.rb
@@ -3,17 +3,17 @@ class Arrest < ApplicationRecord
   belongs_to :crime
 
 def self.find_crimes (state_id, crime_id)
-  
-
-  
     self_oregon_crimes_arr = self.where("state_id = #{state_id} AND crime_id = #{crime_id}")
                             .pluck("year, total_arrests")
                            
-                          stateName= self.find(":state_id").name
-                          crimeName= self.crime.name 
-                           self_oregon_crimes_arr << {"state": stateName,"crime": crimeName}
-                           
-                            self_oregon_crimes_arr.to_h.symbolize_keys
+                          crimeName=  Arrest.where("crime_id=#{crime_id}").first.crime.name
+                          stateName=  Arrest.where("state_id=#{state_id}").first.state.name
+                          # stateName=  Arrest.find(state_id = state_id && crime_id = crime_id).state.name
+                          # crimeName=  Arrest.find(state_id = state_id && crime_id = crime_id).crime.name
+                          # self_oregon_crimes_arr << {"state": stateName,"crime": crimeName}
+                          labelHash = {"#{stateName}:"=> crimeName} 
+                          
+                          return ([self_oregon_crimes_arr.to_h.symbolize_keys,labelHash])
 end 
 
 #   dataArr = Arrest.all.where("state_id = '1'"  && "crime_id = '1'")
